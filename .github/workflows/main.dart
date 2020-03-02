6f6b58fed8cfff658a0b2d07cef44a6e066b1dfe
Muhammadichlas <61673873+Muhammadichlas@users.noreply.github.com>
2020-03-02 10:19:13 +0700

    Add files via upload

---
+++ .github/workflows/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:manajemen_keuangan/splash_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Manajemen Keuangan',
+    home: SplashScreen(),
+  ));
+}
